5ee397ebba5fae0e6e21cc046931c09c8b99e243
Angel <angelememuo@gmail.com>
2023-05-05 11:37:21 +0100

    added mongodb

--- backend/controllers/user-controllers.js
+++ backend/controllers/user-controllers.js
@@ -1,8 +1,12 @@
 import User from "../models/user-model.js"
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 export const signup = async(req, res, next) => {
     const {name,email,password} =   req.body;
+    if (!(name && password && email)) {
+        res.status(400).send("All input is required");
+      }
     let existingUser;
     try {
         existingUser = await User.findOne({email});
@@ -19,36 +23,51 @@ export const signup = async(req, res, next) => {
     const hashedPassword = bcrypt.hashSync(password, salt)
 
     const user = new User({
-        email,
+        email: email.toLowerCase(),
         password: hashedPassword,
         name,
     })
+    const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "2h",
+        }
+      );
+      user.token = token;
     try{
         await user.save()
     }catch(err){
         return console.log(err)
     }
     return res.status(201).json({ user})
+
+   
 }
 
-export const login = async(req,res,next) => {
-    const {email,password} = req.body;
-    let existingUser;
+export const login = async (req, res) => {
     try {
-        existingUser = await User.findOne({email});
-    } catch (err) {
-       return console.log(err);
-    }
-    if(!existingUser){
-        return res
-        .status(404)
-        .json({message: "No User With That Email"});
-    }
-    const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password)
-    if(!isPasswordCorrect) {
-        return res
-       .status(400)
-       .json({message: "Incorrect Password"})
-    } return res.status(200).json({message:"Login Successful."})
+      const { email, password } = req.body;
+      if (!(email && password)) {
+        res.status(400).send("All input is required");
+      }
+      const user = await User.findOne({ email });
+  
+      if (user && (await bcrypt.compare(password, user.password))) {
+        const token = jwt.sign(
+          { user_id: user._id, email },
+          process.env.TOKEN_KEY,
+          {
+            expiresIn: "2h",
+          }
+        );
 
+        user.token = token;
+  
+        res.status(200).json(user);
+      }
+      res.status(400).send("Invalid Credentials");
+    } catch (err) {
+      console.log(err);
+    }
 }
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from 'mongoose'; 
+import User from './backend/models/user-model.js';
 import router from "./backend/routes/user-routes.js";
 import bookingRouter from "./backend/routes/booking-routes.js";
 import bodyParser from "body-parser";
@@ -7,29 +8,32 @@ import pkg from 'body-parser';
 const { json, urlencoded } = pkg
 import cors from 'cors';
 import { config } from "dotenv";
-
+import jwt from 'jsonwebtoken'
 
 
 //core middlewares
 const app = express();
 
-app.use(json())
+app.use(express.json())
 app.use(urlencoded({extended:true}))
 app.use(cors)
 
 //configure env file
 config({path:".env"})
 
-
+app.get('/', (req,res) => {
+    res.json({message: "Hey there! Welcome to this API Service!"});
+})
 
 
 const PORT = process.env.PORT || 4000;
- export function  start() {
- app.listen(PORT,()=>{
- console.log(`Server Listening to port ${PORT} rn`)
- })
+export function  start() {
+    app.listen(PORT,()=>{
+        console.log(`Server Listening on port ${PORT} `)
+    })
 }
-
+mongoose.connect(process.env.DATABASE_URL). then(() => app.listen(PORT)).then(() => console.log('Connected to database '))
+.catch(err =>console.log("Could Not Connect To The Database"))
 
 app.use('/user', router)
 app.use('/booking', bookingRouter)
