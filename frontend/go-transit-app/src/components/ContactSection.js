8b4bf1df8479323fc5e8251bef96184f98fa57db
Joboson Solomon Chiakonam <solobachi02@gmail.com>
2023-05-05 14:04:43 +0100

    finished homepage

---
+++ frontend/go-transit-app/src/components/ContactSection.js
@@ -0,0 +1,18 @@
+function ContactSection(){
+    return(
+    <div className="contactus-bg contactus-wrapper">
+                <div className="bottom-section">
+                    <div className='flex-container'>
+                        <p>Latest travel updates <br/>
+                         Be the first to hear about it
+                        </p>
+                        <form>
+                        <input type="email" placeholder='enter email'/>
+                        <button type='submit' className='cta-btn'>Subscribe</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+    )
+    }
+    export default ContactSection;
